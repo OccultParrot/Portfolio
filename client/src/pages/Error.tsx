aac87ec334789aef11f4410ab39c6aec7c09e4b4
Thomas Stemler <stemlertho@gmail.com>
2025-02-20 20:56:54 -0600

    Blanked out repo

--- client/src/pages/Error.tsx
+++
@@ -1,9 +0,0 @@
-// The home page, where most information is shown.
-export default function Error() {
-
-  return (
-    <div>
-      <p>TEMP</p>
-    </div>
-  )
-}
--- client/src/pages/Home.tsx
+++
@@ -1,8 +0,0 @@
-// The home page, where most information is shown.
-export default function Home() {
-  return (
-    <div>
-      <p>TEMP</p>
-    </div>
-  )
-}
