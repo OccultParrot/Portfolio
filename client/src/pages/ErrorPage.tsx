1fb67e11275168db0470accb940527baab8fffaa
Thomas Stemler <stemlertho@gmail.com>
2025-02-24 13:10:11 -0600

    Filled in mock code for page

--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -1,6 +1,5 @@
 // The home page, where most information is shown.
-export default function Error() {
-
+export default function ErrorPage() {
   return (
     <div>
       <p>ERROR!!!!</p>
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 // The home page, where most information is shown.
-export default function Home() {
+export default function HomePage() {
   return (
     <div>
       <p>Home</p>
