1d36b6156d94c8a2065fcc022c48dd7ffd79cfbd
Thomas Stemler <stemlertho@gmail.com>
2025-02-26 20:03:00 -0600

    Fixed routing bug with child routes I should have tested ages ago lol

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import {BrowserRouter, Outlet, Route, Routes} from 'react-router-dom';
-import {ReactElement} from "react";
+import { Fragment, ReactElement } from 'react';
 import {pages, socials} from './config.tsx';
 import {IPageProps} from './types.ts';
 import ErrorPage from "./pages/ErrorPage.tsx";
 import HeaderSection from "./components/layout/HeaderSection.tsx";
 import FooterSection from "./components/layout/FooterSection.tsx";
+
 
 /**
  * Layout component that wraps all routes with common header and footer elements.
@@ -25,38 +26,6 @@ function Layout(): ReactElement {
       <FooterSection socials={socials}/>
     </div>
   );
-}
-
-/**
- @param props The properties of the Route we want to create. Contains:
-  - **name**: Used for the header menu to travel to that page.
-  - **path**: The path to that page. If child don't write the whole path, just what is added to parent Routes path.
-  - **element**: The React element that the Route will route to.
-  - **children?**: ***Nullable***. An array of more **IPageProps** that represent the children routes.
-
- @returns a **Route** **ReactElement**, for use in the browser router
-
- @desc This function is used for creating the routes for the browser router in the App function
-
- @author Thomas Stemler (OccultParrot)
- **/
-function CreateRoute(props: IPageProps): ReactElement {
-  // If the route has NO children,
-  if (!props.children)
-    // Return a Route element
-    return <Route key={props.path} path={props.path} element={props.element}/>
-
-  // If the route DOES have children,
-  return (
-    // Return a Route element, with children Routes via recursively calling the function
-    <Route key={props.path} path={props.path} element={props.element}>\
-      {/* Map through EACH of the Route's children Routes */}
-      {props.children.map((childProps: IPageProps) =>
-        // Recursively call the CreateRoute function, to make a Route Element here
-        CreateRoute(childProps)
-      )}
-    </Route>
-  )
 }
 
 
@@ -80,15 +49,32 @@ export default function App(): ReactElement {
       <Routes>
         <Route path="/" element={<Layout/>}>
           {/* The "index" page is the one that gets served when you have a blank path */}
-          <Route path={pages[0].path} index element={pages[0].element}/>
+          <Route index element={pages[0].element}/>
 
-          {/* Seeding of paths */}
-          {pages.slice(1).map((pageProps: IPageProps) => CreateRoute(pageProps))}
+          {/* Flatten all routes */}
+          {pages.slice(1).map((pageProps: IPageProps) => {
+            if (!pageProps.children) {
+              return <Route key={pageProps.path} path={pageProps.path} element={pageProps.element} />;
+            }
+
+            return (
+              <Fragment key={pageProps.path}>
+                <Route path={pageProps.path} element={pageProps.element} />
+                {pageProps.children.map((childProps) => (
+                  <Route
+                    key={`${pageProps.path}/${childProps.path}`}
+                    path={`${pageProps.path}/${childProps.path}`}
+                    element={childProps.element}
+                  />
+                ))}
+              </Fragment>
+            );
+          })}
 
           {/* The error page gets served if the path the user goes to does not exist. */}
           <Route path="*" element={<ErrorPage/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
