399ff77dde831f2506131da4f3ae52ad96ced54b
Thomas Stemler <stemlertho@gmail.com>
2025-02-27 08:47:29 -0600

    Added the upload modal for the image hosting application I am using

---
+++ client/src/components/elements/ImageUploader.tsx
@@ -0,0 +1,15 @@
+import { FileUploaderRegular } from '@uploadcare/react-uploader';
+import '@uploadcare/react-uploader/core.css';
+
+function ImageUploaderModal() {
+  return (
+    <FileUploaderRegular
+      sourceList="local, camera, facebook, gdrive"
+      cameraModes="photo"
+      classNameUploader="uc-light"
+      pubkey="3740c4ddeac1e86d2a2c"
+    />
+  );
+}
+
+export default ImageUploaderModal;
