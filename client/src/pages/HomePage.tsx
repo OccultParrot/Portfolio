38f3c0232feac3c85c1e6e91f29e799816bc7d0a
Thomas Stemler <stemlertho@gmail.com>
2025-03-10 12:36:56 -0500

    Added a layout plan for the homepage

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,9 +1,31 @@
+// import {Typewriter} from 'react-simple-typewriter';
+
+{/*<span className="bg-gradient-to-br from-cyan-600 to-cyan-400 bg-clip-text text-5xl font-extrabold  text-transparent">*/}
+{/*  <Typewriter words={["Hello", "World"]} cursor cursorStyle="_" loop={0}/>*/}
+{/*</span>*/}
+
+
+
 // TODO: Finish the home page stuff.
 // The home page, where most information is shown.
 export default function HomePage() {
   return (
     <div>
-      <p>Home</p>
+      {/*  Hero Section */}
+
+      {/* Short value proposition */}
+
+      {/* Logo */}
+
+      {/*  Get in contact button */}
+
+      {/* Skills / Tech Stack */}
+
+      {/* Featured Projects */}
+
+      {/* Brief Introduction, put a link to AboutMe there also */}
+
+      {/* Latest blog post */}
     </div>
   )
 }
