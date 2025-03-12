9815512f76700e7f47c214804aa91a46ed52614f
Thomas Stemler <stemlertho@gmail.com>
2025-03-12 18:10:37 -0500

    Moved the projects array to the config file.

--- client/src/pages/ProjectsPage.tsx
+++ client/src/pages/ProjectsPage.tsx
@@ -1,68 +1,6 @@
 import ProjectCard from '../components/elements/ProjectCard.tsx';
-import {IProjectCardProps} from '../types.ts';
+import {projects} from '../config.tsx';
 
-/**
- * Each project has the following info:
- *
- * title: string - The title of the project
- * description: string - A short description of the project
- * image: string - A link to the image to display (MIGHT CHANGE)
- * isFeatured: bool | null - Optional choice to add it to the featured projects
- * links: IProjectLink - A array of links concerning the project. THE FIRST LINK IS WHAT THE CARD WILL REDIRECT TO
- */
-const projects: IProjectCardProps[] = [
-  {
-    title: 'Friends Without Benefits',
-    description: 'A fun object recognition scavenger hunt, played solo or with a friend!',
-    image: 'https://ucarecdn.com/b5bd864b-904b-401d-b1e7-4ab87c5ec3a4/Screenshot20250302172615.png',
-    links: [
-      {
-        displayName: "Hosted Website",
-        href: "https://friends-without-benefits.onrender.com/",
-      },
-      {
-        displayName: "GitHub Repository",
-        href: "https://github.com/savevsgames/friends-without-benefits"
-      }
-    ]
-  },
-  {
-    title: 'Title',
-    description: 'Blah blah blah blah',
-    image: 'https://www.metroparks.net/wp-content/uploads/2017/06/1080p_HBK_autumn-morning_GI.jpg',
-    isFeatured: true,
-    links: [
-      {
-        displayName: 'Google',
-        href: 'https://www.google.com/'
-      },
-      {
-        displayName: 'Google',
-        href: 'https://www.google.com/'
-      },
-      {
-        displayName: 'Google',
-        href: 'https://www.google.com/'
-      },
-      {
-        displayName: 'Google',
-        href: 'https://www.google.com/'
-      }
-    ]
-  },
-  {
-    title: 'Title',
-    description: 'Blah blah blah blah',
-    image: '',
-    // isFeatured: true,
-    links: [
-      {
-        displayName: 'Google',
-        href: 'https://www.google.com/'
-      }
-    ]
-  }
-];
 
 export default function ProjectsPage() {
   const featuredProjects = projects.filter((project) => !!project.isFeatured);
