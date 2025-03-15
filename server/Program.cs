73a3f01d0bc2e256121221abb20f2831ebeb991c
Thomas Stemler <stemlertho@gmail.com>
2025-03-15 10:29:53 -0500

    Made have the portfolio context as well as a message letting you know that you can test the API in development environent via route/swagger

--- server/Program.cs
+++ server/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using server.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,16 +8,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<PortfolioContext>(opt => {
+  opt.UseInMemoryDatabase("ContactList");
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseSwaggerUi(options => {
-      options.DocumentPath = "/openapi/v1.json";
-    });
+if (app.Environment.IsDevelopment()) {
+  app.MapOpenApi();
+  app.UseSwaggerUi(options => { options.DocumentPath = "/openapi/v1.json"; });
+  
+  Console.BackgroundColor = ConsoleColor.Yellow;
+  Console.ForegroundColor = ConsoleColor.Black;
+  Console.WriteLine("Go to /swagger to see the Swagger UI for testing the API.");
+  Console.ResetColor();
 }
 
 app.UseHttpsRedirection();
