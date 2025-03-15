5a660a61ddef1c2f6decce37d33bc91c43944bb6
Thomas Stemler <stemlertho@gmail.com>
2025-03-15 10:28:53 -0500

    Added models for our contact messages and portfolio cards

--- server/Models/ContactItem.cs
+++ server/Models/ContactItem.cs
@@ -1,5 +1,8 @@
 ﻿namespace server.Models;
 
 public class ContactItem {
-  
+  public long Id { get; set; }
+  public string Name { get; set; } = null!;
+  public string Email { get; set; } = null!;
+  public string Message { get; set; } = null!;
 }
--- server/Models/ProjectItem.cs
+++ server/Models/ProjectItem.cs
@@ -1,5 +1,16 @@
-namespace server.Models;
+﻿namespace server.Models;
 
 public class ProjectItem {
   public long Id { get; set; }
+  public string Title { get; set; } = null!;
+  public string Description { get; set; } = null!;
+  public bool IsFeatured { get; set; }
+  public string Image { get; set; } = null!;
+  public ProjectLink[] Links { get; set; } = null!;
+}
+
+public class ProjectLink {
+  public long Id { get; set; }
+  public string Title { get; set; } = null!;
+  public string Link { get; set; } = null!;
 }
