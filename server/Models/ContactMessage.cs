801efb4e8d58d921c145275cbe1ac240556aaf29
Thomas Stemler <stemlertho@gmail.com>
2025-03-29 17:54:11 -0500

    Moved client files to root, and deleted server folder because we dont need a server anymore. Emails are being done entirely on the client

--- server/Models/ContactMessage.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace server.Models;
-
-public class ContactMessage {
-  public long Id { get; set; }
-  public string Name { get; set; } = null!;
-  public string Email { get; set; } = null!;
-  public string Message { get; set; } = null!;
-}
